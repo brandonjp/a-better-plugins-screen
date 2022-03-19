ba5b9c11b442c2bd7119e1f5a42db6f565c5c5ee
Brandon Pfeiffer <brandonjp@gmail.com>
2022-03-19 17:32:26 -0500

    Fixes #1: check that hosts match, separate file from fileVariations searches, change order of scan types

--- a_better_plugins_screen.js
+++ a_better_plugins_screen.js
@@ -4,8 +4,8 @@
   $(window).on('load',function () {
 
     // Optional debug output we'll use later, if desired
-    let debugOn = false;
-
+    let debugOn = true;
+    
     // Some plugins have names that don't match their slugs/urls
     // Here is a manual dictionary for matching those
     // Add new ones in alphabetical order as `slug:url`
@@ -21,35 +21,65 @@
           .indexOf(m[3].toUpperCase()) >= 0;
     };
 
+    // function to get the host of a url string
+    let returnHost = function returnHost(urlString){
+      let thisHost;
+      let url = (urlString || '');
+      if (url.length) {
+        let a = document.createElement('a');
+        a.href = url;
+        thisHost = a.host;
+      }
+      return (thisHost || '');
+    }
+    // function to check if the host of a url matches current site
+    let hostsMatch = function hostsMatch(urlString){
+      let siteHost = window.location.host;
+      let urlHost = returnHost(urlString || '');
+      return siteHost===urlHost;
+    }
+
     // function we'll use for various scanning methods
     let scanForPluginSettings = function scanForPluginSettings(scanType,plugData,debugOn){
-      let returnValue;
+      let returnValue, matchFound;
       let $adminMenu = jQuery('ul#adminmenu');
+      // try looking up the slug in the manual dictionary
       if (scanType==='custom') {
         if (debugOn) console.log(`[${plugData.name}] Looking for a match in custom locations.`);
-        returnValue = betterLinks[plugData.slug];
+        matchFound = betterLinks[plugData.slug]; 
       }
+      // try looking in the description column
       if (scanType==='description') {
         // Occasionally a developer will put their own Settings link in the plugin description
         // right now we're only looking for a link that contains the text string 'settings'
-        if (debugOn) console.log(`[${plugData.name}] Looking for a match in the description column.`);
-        returnValue = plugData['row'].find(`a:icontains("settings")`).attr('href');
+        if (debugOn) console.log(`[${plugData.name}] Looking for a match in the description.`);
+        matchFound = plugData['row'].find(`a:icontains("settings")`).attr('href');
       }
+      // try matching the plugin Name/Title in the menus
       if (scanType==='name') {
         if (debugOn) console.log(`[${plugData.name}] Looking for a match with name: ${plugData.name}`);
-        returnValue = $adminMenu.find(`a:contains("${plugData.name}")`).attr('href');
+        matchFound = $adminMenu.find(`a:contains("${plugData.name}")`).attr('href');
       }
+      // try matching the slug to an href in the admin menu
       if (scanType==='slug') {
         if (debugOn) console.log(`[${plugData.name}] Looking for a match with slug: ${plugData.slug}`);
-        returnValue = $adminMenu.find(`a[href*="${plugData.slug}"]`).attr('href');
+        matchFound = $adminMenu.find(`a[href*="${plugData.slug}"]`).attr('href');
       }
-      if (scanType==='plug') {
-        if (debugOn) console.log(`[${plugData.name}] Looking for a match with plugin file: ${plugData.plug}`);
-        returnValue = $adminMenu.find(`a[href*="${plugData.plug}"]`).attr('href');
+      // try matching the plugin's filename
+      if (scanType==='file') {
+        // first search for a direct match with the plugin's full folder/file.php 
+        if (debugOn) console.log(`[${plugData.name}] Looking for a match with filename: ${plugData.file}`);
+        matchFound = $adminMenu.find(`a[href*="${plugData.file}"]`).attr('href');
+      }
+      // try matching variations of the plugin's filename
+      if (scanType==='fileVariations') {
+        // first search for a direct match with the plugin's full folder/file.php 
+        if (debugOn) console.log(`[${plugData.name}] Looking for a match with filename variations: ${plugData.file}`);
+        matchFound = $adminMenu.find(`a[href*="${plugData.file}"]`).attr('href');
         // check for variations
-        if (!returnValue || !returnValue.length) {
+        if (!matchFound || !matchFound.length) {
           // take a plugin path 'this-folder/this-file.php' and return array ['this-file','this-folder']
-          let stringParts = plugData.plug.replaceAll('.php','').split('/');
+          let stringParts = plugData.file.replaceAll('.php','').split('/');
           if (stringParts && stringParts.length) {
             // start by amending the array with other possible variations
             stringParts.forEach(function(l,i){
@@ -62,15 +92,23 @@
               let withoutSeparators = l.replaceAll('-','').replaceAll('_','');
               stringParts.push(withoutSeparators);
             });
+            // remove duplicates from the array
+            let stringPartsSet = [...new Set(stringParts)];
             // then check each variation in the array for a match
-            stringParts.forEach(function(ll,ii){
-              if (debugOn) console.log(`[${plugData.name}] Looking for a match with string: ${ll}`);
-              returnValue = $adminMenu.find(`a[href*="${ll}"]`).attr('href');
-              if (returnValue) return returnValue;
+            stringPartsSet.forEach(function(ll,ii){
+              if (debugOn) console.log(`[${plugData.name}] Looking for a match with variation string #${ii}: ${ll}`);
+              matchFound = $adminMenu.find(`a[href*="${ll}"]`).attr('href');
+              if (matchFound && hostsMatch(matchFound)) {
+                if (debugOn) console.log(`[${plugData.name}] Found a match with variation string #${ii}: ${ll} --  Match: ${matchFound}`);
+                return matchFound;
+              } else if (matchFound && !hostsMatch(matchFound)) {
+                if (debugOn) console.log(`[${plugData.name}] Found a match but Hosts to do not match #${ii}: ${ll} --  Match: ${matchFound}`);
+              }
             });
           }
         }
       }
+      if (hostsMatch(matchFound)) returnValue = matchFound;
       return returnValue;
     }
 
@@ -81,7 +119,7 @@
       // Slug we'll use to find the settings page
       let slug = $thisRow.data('slug');
       // Plugin path, we probably won't use this
-      let plug = $thisRow.data('plugin');
+      let file = $thisRow.data('plugin');
       // Name of the plugin
       let name = $thisRow.find('td.plugin-title strong').text();
       // The existing row actions wrapper
@@ -97,35 +135,38 @@
       let plugData = {
         'name': name,
         'slug': slug,
-        'plug': plug,
+        'file': file,
         'row' : $thisRow
       };
       // If there's no existing Settings link, try to locate & build one
       if (!settingsHref || !settingsHref.length) {
-        if (debugOn) console.log(`Found: ${name}, ${slug}, ${plug}`);
+        if (debugOn) console.log(`Name: ${name}`,`Slug: ${slug}`,`File: ${file}`);
         if (debugOn) console.log(`[${name}] No default Settings link found. Looking for matches.`);
         
         // First, look directly for the slug in the admin menu
         settingsHref = scanForPluginSettings('slug',plugData,debugOn);
 
+        // If we didn't find a url, try looking up the slug in the manual dictionary
+        if (!settingsHref || !settingsHref.length) settingsHref = scanForPluginSettings('custom',plugData,debugOn);
+
         // If we didn't find a url, try matching the plugin file
-        if (!settingsHref || !settingsHref.length) settingsHref = scanForPluginSettings('plug',plugData,debugOn);
+        if (!settingsHref || !settingsHref.length) settingsHref = scanForPluginSettings('file',plugData,debugOn);
 
         // If we didn't find a url, try matching the plugin Name/Title
         if (!settingsHref || !settingsHref.length) settingsHref = scanForPluginSettings('name',plugData,debugOn);
 
-        // If we still didn't find a url, try looking in the description column
+        // If we didn't find a url, try looking in the description column
         if (!settingsHref || !settingsHref.length) settingsHref = scanForPluginSettings('description',plugData,debugOn);
 
-        // If we still didn't find a url, try looking up the slug in the manual dictionary
-        if (!settingsHref || !settingsHref.length) settingsHref = scanForPluginSettings('custom',plugData,debugOn);
+        // If we didn't find a url, try matching the plugin file
+        if (!settingsHref || !settingsHref.length) settingsHref = scanForPluginSettings('fileVariations',plugData,debugOn);
 
         // If we found a url for the Settings page, build the link
         if (settingsHref && settingsHref.length) {
           if (debugOn) console.log(`[${name}] Found settings link at: ${settingsHref}`);
           $settingsLink = jQuery(`<span class="settings a-better-plugins-screen-link"><a href="${settingsHref}">Settings</a></span>`);
         } else {
-          if (debugOn) console.log(`[${name}] Could not find a Settings link for ${plug}.`);
+          if (debugOn) console.log(`[${name}] Could not find a Settings link for ${file}.`);
           $settingsLink = jQuery(`<span class="settings a-better-plugins-screen-link">No Settings Found</span>`);
         }
       }
@@ -144,8 +185,9 @@
       // Add some debug info to each row, if enabled
       if (debugOn) {
         let debugInfo = {
+          'Name': name,
           'Slug': slug,
-          'Plugin': plug,
+          'File': file,
           'Settings': settingsHref,
         };
         jQuery.each(debugInfo, function (k, v) {
